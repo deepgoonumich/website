19643d1b043de03616780c9a27085d161808742a
dipanjan <dipanjan@umich.edu>
2019-08-05 13:12:07 -0400

    random stuff

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -3,8 +3,21 @@ import React from 'react'
 function About () {
     return (
         <header>
-            About Me!
-        </header>
+            <iframe src="https://open.spotify.com/embed/playlist/5NdhPP7ZMwEUmUS36aZqNW" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+            <br />
+            <iframe src="https://open.spotify.com/embed/playlist/7vqIfKH6ib4o0N8Vk6lRhO" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+           
+           <br />
+           <br />
+
+           <iframe
+            width="600"
+            height="450"
+            frameborder="0" 
+            src="https://www.google.com/maps/place/723+Packard+St,+Ann+Arbor,+MI+48104/@42.270439,-83.7418793,17z/data=!3m1!4b1!4m5!3m4!1s0x883cae484845b5cd:0xafe15348eca8ad92!8m2!3d42.2704351!4d-83.7396906" allowfullscreen>
+            </iframe>
+
+         </header>   
     )
 }
 
