f6de197d5d38b01aed48b4c1adf93c74341a05de
deep <dipanjan@umich.edu>
2019-10-10 15:30:49 -0400

    for pb

--- my-app/src/components/Footer.js
+++ my-app/src/components/Footer.js
@@ -1,13 +1,40 @@
-import React from 'react'
+import React from 'react';
 import '../styles/Footer.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faGithub,
+  faLinkedin
+} from "@fortawesome/free-brands-svg-icons";
 
 
-function Footer () {
-    return (    
-        <footer className="foot">
-            <p>Some footer nonsense!</p>
-        </footer>
-    )
+const Footer = () => {
+  return (
+    <div className="social-container">
+      <a
+        href="https://github.com/deepgoonumich" target="_blank" rel="noopener noreferrer"
+        className="github social"
+      >
+        <FontAwesomeIcon icon={faGithub} size="2x" />
+      </a>
+      <a
+        href="https://www.facebook.com/DeEpGOon" target="_blank" rel="noopener noreferrer"
+        className="facebook social"
+      >
+        <FontAwesomeIcon icon={faFacebook} size="2x" />
+      </a>
+      <a href="https://www.linkedin.com/in/deepgoon/"  target="_blank" rel="noopener noreferrer" className="linkedin social">
+        <FontAwesomeIcon icon={faLinkedin} size="2x" />
+      </a>
+      <a
+        href="https://www.instagram.com/deepgoon_/" target="_blank" rel="noopener noreferrer"
+        className="instagram social"
+      >
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
+      </a>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
